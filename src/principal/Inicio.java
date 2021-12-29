7c85ccf111e568012ec78fc5721f1186a0b67271
hecpabe <hectorpbenavides@gmail.com>
2021-12-29 18:41:39 +0100

    Inicio del Proyecto

---
+++ src/principal/Inicio.java
@@ -0,0 +1,17 @@
+
+
+/*
+ * 	Título: Inicio
+ * 	Autores: Héctor Paredes Benavides / Sergio Bermúdez Fernández
+ * 	Descripción: Clase que inicializa el programa
+ */
+
+package principal;
+
+public class Inicio {
+
+	public static void main(String args[]){
+		
+	}
+	
+}
